99e48af9f938d513409c47f3a7a9254779c0e5c7
pkcoder471 <pklid471@gmail.com>
2022-10-31 21:05:11 +0530

    setting up ejs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const port=8000;
 const app=express();
 
 app.use('/',require('./routes'));
-
+app.set('view engine','ejs');
+app.set('views','./views')
 app.listen(port,function(err){
     if(err){
         console.log(`error in creating server :${err}`);

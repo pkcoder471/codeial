99d3fc41869c54880946989feeac630372abae74
pkcoder471 <pklid471@gmail.com>
2022-10-31 21:57:21 +0530

    setting up layout and mongoose

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express= require('express');
 const port=8000;
 const app=express();
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/',require('./routes'));
+
 app.set('view engine','ejs');
 app.set('views','./views')
+
 app.listen(port,function(err){
     if(err){
         console.log(`error in creating server :${err}`);

7820ade7f5f9150ce809f8b6342bd03deea8c0df
pkcoder471 <pklid471@gmail.com>
2022-10-31 22:26:01 +0530

    Sign-in and Sign-up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,14 @@
 module.exports.profile=function(req,res){
-    return res.end("<h1>My Profile<h1>");
+    return res.render("<h1>My Profile<h1>");
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"codeial | SignIn"
+    })
+}
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"codeial | SignUp"
+    })
 }

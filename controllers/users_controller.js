3e9dfb198ba18dcd0c1301c84276161fb4c4e26b
pkcoder471 <pklid471@gmail.com>
2022-11-01 18:13:02 +0530

    user sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+
+const User=require('../models/user')
+
 module.exports.profile=function(req,res){
     return res.render("<h1>My Profile<h1>");
 }
@@ -11,4 +14,25 @@ module.exports.signUp=function(req,res){
     return res.render('user_sign_up',{
         title:"codeial | SignUp"
     })
+}
+
+module.exports.create=function(req,res){
+     if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+     }
+
+     User.findOne({email:req.body.email},function(err,user){
+          if(err){ console.log(`error in finding user :${err}`); return;}
+
+          if(!user){
+              User.create(req.body,function(err,user){
+                if(err){ console.log(`error in creating user :${err}`); return;}
+
+                return res.redirect('/users/sign-in');
+              })
+          }
+          else{
+            return res.redirect('back');
+          }
+     })
 }

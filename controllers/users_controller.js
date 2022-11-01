eff6747aa880673543780d1be209cc66a4375ed5
pkcoder471 <pklid471@gmail.com>
2022-11-01 19:18:48 +0530

    manual auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,21 @@
+
+const User=require('../models/user')
+
 module.exports.profile=function(req,res){
-    return res.render("<h1>My Profile<h1>");
+    if(req.cookies.user_id){
+         User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('profile',{
+                    title:"User_profile",
+                    user:user
+                });
+            }
+            else{
+                return res.redirect('/users/sign-in');
+            }
+         })
+        }
+    
 }
 
 module.exports.signIn=function(req,res){
@@ -10,5 +26,46 @@ module.exports.signIn=function(req,res){
 module.exports.signUp=function(req,res){
     return res.render('user_sign_up',{
         title:"codeial | SignUp"
+    })
+}
+
+module.exports.create=function(req,res){
+     if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+     }
+
+     User.findOne({email:req.body.email},function(err,user){
+          if(err){ console.log(`error in finding user :${err}`); return;}
+
+          if(!user){
+              User.create(req.body,function(err,user){
+                if(err){ console.log(`error in creating user :${err}`); return;}
+
+                return res.redirect('/users/sign-in');
+              })
+          }
+          else{
+            return res.redirect('back');
+          }
+     })
+}
+
+module.exports.createSession=function(req,res){
+    User.findOne({email:req.body.email},function(err,user){
+        
+            
+        if(err){console.log(`error in finding user :${err}`);  return;}
+         
+        if(user){
+            if(req.body.password!=user.password){
+                return res.redirect('back');
+             }
+
+            res.cookie('user_id',user.id);
+            res.redirect('/users/profile');
+        }
+        else{
+            res.redirect('back');
+        }
     })
 }

10f46afd30772c0c50cdb84f4539738f68584832
pkcoder471 <pklid471@gmail.com>
2022-11-01 22:25:10 +0530

    authentication using passport.js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -34,4 +34,18 @@ passport.deserializeUser(function(id,done){
     })
 })
 
+passport.checkAuthentication=function(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser=function(req,res,next){
+    if(req.isAuthenticated()){
+        res.locals.user=req.user;
+    }
+    next();
+}
 module.exports=passport;

740ff7c4dc572569ab084f023407b8c1036c02d8
pkcoder471 <pklid471@gmail.com>
2022-12-12 23:48:33 +0530

    updating users profile and css

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,6 +11,17 @@ module.exports.profile=function(req,res){
     
 }
 
+module.exports.update=function(req,res){
+    if(req.user.id==req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            return res.redirect('back');
+        })
+    }
+    else{
+        res.status(401).send('unauthorized');
+    }
+}
+
 module.exports.signIn=function(req,res){
     if(req.isAuthenticated()){
         return res.redirect('/users/profile');
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 const router=express.Router();
 const userController=require('../controllers/users_controller')
 
-router.get('/profile/:id',passport.checkAuthentication,userController.profile)
+router.get('/profile/:id',passport.checkAuthentication,userController.profile);
+router.post('/update/:id',passport.checkAuthentication,userController.update);
 
 router.get('/sign-in',userController.signIn);
 router.get('/sign-up',userController.signUp);

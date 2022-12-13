d88bfdde6e7671f653d959d5b7a4b3b13c35b5b9
pkcoder471 <pklid471@gmail.com>
2022-12-14 00:33:39 +0530

    noty js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -4,16 +4,17 @@ const localStrategy=require('passport-local').Strategy;
 const User=require('../models/user')
 
 passport.use(new localStrategy({
-    usernameField:'email'
+    usernameField:'email',
+    passReqToCallback:true
 },
-function(email,password,done){
+function(req,email,password,done){
     User.findOne({email:email},function(err,user){
         if(err){
-            console.log("error in finding user");
+            req.flash('error',err);
             return done(err);
         }
         if(!user || user.password!=password){
-            console.log('error in signing in');
+            req.flash('error','Invalid Username/Password');
             return done(null,false)
         }
         return done(null,user);

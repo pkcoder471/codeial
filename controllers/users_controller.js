fe4a845dd83996ed78e0b3362e94a528109f8759
pkcoder471 <pklid471@gmail.com>
2022-12-14 22:23:21 +0530

    config multer and saving files

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,11 +11,26 @@ module.exports.profile=function(req,res){
     
 }
 
-module.exports.update=function(req,res){
+module.exports.update= async function(req,res){
     if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+        try{
+        let user = await User.findByIdAndUpdate(req.params.id);
+        User.uploadedAvatar(req,res,function(err){
+            if(err){console.log('****Multer Error',err)}
+            
+            user.name= req.body.name;
+            user.email= req.body.email;
+
+            if(req.file){
+                user.avatar= User.avatarPath+'/'+req.file.filename;
+            }
+            user.save();
             return res.redirect('back');
         })
+        }
+        catch(err){
+            return res.redirect('back');
+        }
     }
     else{
         res.status(401).send('unauthorized');

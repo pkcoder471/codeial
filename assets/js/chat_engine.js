44e510a6f808402418ec112f4319d433fa80797a
pkcoder471 <pklid471@gmail.com>
2022-12-22 20:55:34 +0530

    send and receive message

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -24,6 +24,45 @@ class chatEngine{
             self.socket.on('user_joined',function(data){
                 console.log('a user joined',data);
             })
+        });
+        
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+            console.log(msg);
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+
+        self.socket.on('receive_message',function(data){
+            console.log('message received',data.message);
+
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if(data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>',{
+                'html': data.message
+            }));
+
+            newMessage.append($('<sub>',{
+                'html': data.userEmail
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
         })
+
     }
+
+    
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -13,6 +13,10 @@ module.exports.chatSockets = function(socketServer){
             console.log('joining request rec',data);
             socket.join(data.chatroom);
             io.in(data.chatroom).emit('user_joined',data);
+        }) 
+
+        socket.on('send_message',function(data){
+            io.in(data.chatroom).emit('receive_message',data)
         })
     });
 

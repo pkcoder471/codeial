03f0e627e84d82e238e01cafec46e276ccb3f8ff
pkcoder471 <pklid471@gmail.com>
2022-12-22 19:27:29 +0530

    setting up socket.io

---
+++ config/chat_sockets.js
@@ -0,0 +1,20 @@
+module.exports.chatSockets = function(socketServer){
+
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection',function(socket){
+        console.log('new connect recived',socketid);
+
+        sockets.on('disconnect',function(){
+            console.log('socket disconnected');
+        });
+
+        sockets.on('join room',function(data){
+            console.log('joining request rec',data);
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined',data);
+        })
+    });
+
+
+}
